8ad330b54bc815c2f613fb82e8122f7134485445
brutalKim <132333588+brutalKim@users.noreply.github.com>
2026-09-09 23:03:35 +0900

    fix

--- lib/discord/discord-commands.ts
+++ lib/discord/discord-commands.ts
@@ -1,7 +1,6 @@
 import "server-only";
 
 import { boboKingCommand } from "@/lib/discord/commands/bobo-king-command";
-import { broadcastMessageCommand } from "@/lib/discord/commands/broadcast-message-command";
 import { pingCommand } from "@/lib/discord/commands/ping-command";
 import { recentWinCommand } from "@/lib/discord/commands/recent-win-command";
 import { registerPlayerCommand } from "@/lib/discord/commands/register-player-command";
@@ -11,7 +10,6 @@ import type { DiscordCommand } from "@/lib/discord/discord-command";
 /** 등록과 런타임 라우팅이 함께 사용하는 단일 명령 목록이다. */
 export const discordCommands: readonly DiscordCommand[] = [
   pingCommand,
-  broadcastMessageCommand,
   boboKingCommand,
   teamSplitCommand,
   recentWinCommand,
